c78cb5f5de380e41e186d53890ca05f22287b059
Elder Baltazar <Elder Baltazar>
2016-11-20 00:21:21 -0300

    mais sensivel ainda

--- app/src/main/java/br/com/startupweek/speciall/main/MainActivity.java
+++ app/src/main/java/br/com/startupweek/speciall/main/MainActivity.java
@@ -1,8 +1,10 @@
 package br.com.startupweek.speciall.main;
 
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -49,35 +51,72 @@ public class MainActivity extends AppCompatActivity {
 
     private void calculateLineLength(float x, float y) {
 
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+        int maxY = (int)(height * 0.9);
+        int minY = (int)(height * 0.1);
+        int minLineSize = (int)(height *0.6);
+        int maxLineSize = (int)(height *0.9);
+
+
         double b = Math.pow((int)y - (int)lastY, 2);
         double c = Math.pow((int)x - (int)lastX, 2);
         int lenght = (int)Math.sqrt(b + c);
-        Log.i("LENGHT", "L = " + lenght);
 
-        if((int)lastY > (int)y) {
-            if ((int) lastY <= 1400 && (int) y >= 50 && lenght <= 1300 && lenght >= 1100) {
-                Toast.makeText(this,"LY > y Good Job!",Toast.LENGTH_SHORT).show();
+        Log.i("TAG", "W " + width + " H " + height);
+        Log.i("TAG", "L = " + lenght);
+        Log.i("TAG", "minY = " + minY);
+        Log.i("TAG", "maxY = " + maxY);
+        Log.i("TAG", "minLineSize = " + minLineSize);
+        Log.i("TAG", "maxLineSize = " + maxLineSize);
+        Log.i("TAG", "success X " + (int) x + " LX " + (int) lastX);
+        Log.i("TAG", "success Y " + (int) y + " LY" + (int) lastY);
+
+//        I/TAG: W 1080 H 1776
+//        I/TAG: L = 1288
+//        I/TAG: minY = 177
+//        I/TAG: maxY = 1598
+//        I/TAG: minLineSize = 1243
+//        I/TAG: maxLineSize = 1598
+//        I/TAG: success X 556 LX 67
+//        I/TAG: success Y 102 LY 1294
+//        I/TAG: success X 67 556 1192
+
+//        I/TAG: W 720 H 1184
+//        I/TAG: L = 702
+//        I/TAG: minY = 118
+//        I/TAG: maxY = 1065
+//        I/TAG: minLineSize = 828
+//        I/TAG: maxLineSize = 1065
+//        I/TAG: success X 405 LX 83
+//        I/TAG: success Y 151 LY 775
+//        I/TAG: success X 83 405 624
+
+        if(lenght <= maxLineSize && lenght >= minLineSize)
+        {
+
+            if ((int) y < (int) lastY && (int) x > lastX) {
+                Toast.makeText(this, "LY > y Good Job!", Toast.LENGTH_SHORT).show();
                 lines++;
-//                Log.i("TAG", "success down up");
-//                Log.i("TAG", "success X " + (int) x + " " + (int) lastX);
-//                Log.i("TAG", "success Y" + (int) y + " " + (int) lastY);
+            }
+            else if ((int) y > (int)lastY && (int)x > lastX )
+            {
+                Toast.makeText(this, "Good Job!", Toast.LENGTH_SHORT).show();
+                lines++;
             }
         }
-        else if((int)y > (int)lastY) {
-            if ((int) y <= 1400 && (int) lastY >= 50 && lenght <= 1300 && lenght >= 1100) {
-                Toast.makeText(this,"Good Job!",Toast.LENGTH_SHORT).show();
-                lines++;
-//                Log.i("TAG", "success up down");
-//                Log.i("TAG", "success X " + (int) x + " " + (int) lastX);
-//                Log.i("TAG", "success Y" + (int) y + " " + (int) lastY);
-            }
+        else if( (int)x < lastX && Math.abs(y - lastY) < 80){
+            Toast.makeText(this, "Good Job!", Toast.LENGTH_SHORT).show();
         }
+    }
 
-
-        if ( (int)lastX > (int)x ){//&& Math.abs((int)lastY - (int)y)  < 50){
-                Toast.makeText(this,"Good Job!",Toast.LENGTH_SHORT).show();
-        }
-                Log.i("TAG", "success X " + (int)lastX  + " " + (int)x + " " +Math.abs((int)lastY - (int)y));
+//        if ( (int)lastX > (int)x ){//&& Math.abs((int)lastY - (int)y)  < 50){
+//                Toast.makeText(this,"Good Job!",Toast.LENGTH_SHORT).show();
+//        }
+//        Log.i("TAG", "success X " + (int)lastX  + " " + (int)x + " " +Math.abs((int)lastY - (int)y));
 
 //        switch (counter){
 //            case 0:
@@ -90,6 +129,6 @@ public class MainActivity extends AppCompatActivity {
 //            case 2:
 //                return;
 //        }
-    }
+//    }
 
 }
